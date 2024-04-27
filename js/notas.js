4185788a0cce4eff74295d567dbd6dca4d270722
artemFedossov <a.fedossov@hotmail.com.ar>
2024-04-27 15:05:19 -0300

    se agrego color y falta dar estilos

---
+++ js/notas.js
@@ -0,0 +1,20 @@
+const btnAgregar = document.querySelector('#agregar');
+const btnBorrar = document.querySelector('#borrar');
+const contenedor = document.querySelector('#contenedor');
+
+let bloqueNotas = [];
+
+btnAgregar.addEventListener('click', ()=>{
+    let nuevaNota = document.createElement('input');
+    nuevaNota.type = "text";
+    nuevaNota.className = "nota";
+    contenedor.appendChild(nuevaNota);
+    bloqueNotas.push(nuevaNota);
+})
+
+btnBorrar.addEventListener('click', ()=>{
+    if(bloqueNotas.length > 0){
+        let ultimaNota = bloqueNotas.pop();
+        contenedor.removeChild(ultimaNota);
+    }
+})
