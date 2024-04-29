a9d7538d0c5ee40ee883abac097bd5d0ee4875d5
artemFedossov <a.fedossov@hotmail.com.ar>
2024-04-29 15:01:27 -0300

    limpiando el codigo

--- js/notas.js
+++ js/notas.js
@@ -1,10 +1,14 @@
+// Selecciona los botones y el contenedor de las notas en el HTML
 const btnAgregar = document.querySelector('#agregar');
 const btnBorrar = document.querySelector('#borrar');
 const contenedor = document.querySelector('#contenedor');
-
+// Un array para almacenar las notas
 let bloqueNotas = [];
 
-btnAgregar.addEventListener('click', ()=>{
+// Evento click para el boton de agregar notas de forma dinamica
+btnAgregar.addEventListener('click', () => {
+    // Crea un nuevo elemento input, asignandole propiedades
+    // y lo agrega al array
     let nuevaNota = document.createElement('input');
     nuevaNota.type = "text";
     nuevaNota.className = "nota";
@@ -13,8 +17,10 @@ btnAgregar.addEventListener('click', ()=>{
     bloqueNotas.push(nuevaNota);
 })
 
-btnBorrar.addEventListener('click', ()=>{
-    if(bloqueNotas.length > 0){
+// Evento click para el boton de borrar
+btnBorrar.addEventListener('click', () => {
+    // Verifica si hay notas en el array y borra la última
+    if (bloqueNotas.length > 0) {
         let ultimaNota = bloqueNotas.pop();
         contenedor.removeChild(ultimaNota);
     }
--- js/traductor.js
+++ js/traductor.js
@@ -1,18 +1,19 @@
-const getUrl = 'https://text-translator2.p.rapidapi.com/getLanguages';
+// URL de la API para obtener la lista de lenguajes disponibles y para   
+// traducir el texto
+const getIdioma = 'https://text-translator2.p.rapidapi.com/getLanguages';
 const postUrl = 'https://text-translator2.p.rapidapi.com/translate';
-
+// Elementos del HTML que se utilizan en el codigo
 const selectA = document.querySelector('#selectA');
 const selectB = document.querySelector('#selectB');
 const botonTraducir = document.querySelector('#traducir');
 const entradaTexto = document.querySelector('#entradaTexto');
 const salidaTexto = document.querySelector('#salidaTexto');
-
+// Variable para almacenar los lenguajes disponibles 
 let lenguajes = [];
 let sourceLenguaje;
 let targetLenguaje;
-let opcionA;
-let opcionB;
 
+// Opciones para la solicitud GET a la API
 const optionsGet = {
 	method: 'GET',
 	headers: {
@@ -21,39 +22,45 @@ const optionsGet = {
 	}
 };
 
-
-fetch(getUrl, optionsGet)
+// Realiza la solicitud GET para obtener los lenguajes disponibles
+fetch(getIdioma, optionsGet)
 	.then(response => response.json())
 	.then(data => {
-
+		// Almacena los lenguajes disponibles en la variable lenguajes
 		lenguajes = data.data.languages;
 
+		// Itera sobre los lenguajes y crea opciones para los select en el HTML
 		lenguajes.forEach(function (lenguaje) {
 			if ((lenguaje.code === "en") || (lenguaje.code === "fr") ||
 				(lenguaje.code === "it") || (lenguaje.code === "pt") ||
 				(lenguaje.code === "ru") || (lenguaje.code === "es")) {
-				opcionA = document.createElement('option')
-				opcionB = document.createElement('option')
+				// Crea una opción para el select de origen
+				let opcionA = document.createElement('option')
 				opcionA.textContent = lenguaje.name;
 				opcionA.value = lenguaje.code;
+				selectA.appendChild(opcionA);
+
+				// Crea una opción para el select de destino
+				let opcionB = document.createElement('option')
 				opcionB.textContent = lenguaje.name;
 				opcionB.value = lenguaje.code;
-				selectA.appendChild(opcionA);
 				selectB.appendChild(opcionB);
 			}
 		});
 
+		// Evento para seleccionar el lenguaje de origen
 		selectA.addEventListener('click', () => {
 			sourceLenguaje = selectA.value;
 		})
-
+		// Evento para seleccionar el lenguaje de destino
 		selectB.addEventListener('click', () => {
 			targetLenguaje = selectB.value;
 		})
 	})
 
+// Evento click para el botón de traducir
 botonTraducir.addEventListener('click', () => {
-
+	// Opciones para la solicitud POST a la API
 	const optionsPost = {
 		method: 'POST',
 		headers: {
@@ -67,14 +74,15 @@ botonTraducir.addEventListener('click', () => {
 			text: entradaTexto.value
 		})
 	};
-
+	// Realiza la solicitud POST para traducir el texto
 	fetch(postUrl, optionsPost)
 		.then(response => response.json())
 		.then(data => {
+			// Coloca el texto traducido en el área de salida en el HTML
 			salidaTexto.value = data.data.translatedText;
 		})
 		.catch(err => {
-
+			// Si no se puede realizar la traducción, muestra un mensaje de error
 			if ((selectA.value === "Seleccione Idioma") || (selectB.value === "Seleccione Idioma")) {
 				Swal.fire({
 					title: "No se pudo traducir",
